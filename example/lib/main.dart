eef209001cf13fb591b6125008f48378009775da
Tomohiro-Uchida <uchida001tmhr@indigosoftware.org>
2023-11-15 19:44:58 +0900

    1.0.1

--- example/lib/main.dart
+++ example/lib/main.dart
@@ -1,7 +1,6 @@
 import 'package:flutter/material.dart';
 import 'dart:async';
 
-import 'package:flutter/services.dart';
 import 'package:flutter_sharing_intent/flutter_sharing_intent.dart';
 import 'package:flutter_sharing_intent/model/sharing_file.dart';
 import 'package:read_text_content_from_uri/read_text_content_from_uri.dart';
